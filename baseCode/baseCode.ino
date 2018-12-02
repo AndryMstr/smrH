5e94d57aff19169fa240f3f3dfa4faea4924412d
AndryMstr <a9647961157@gmail.com>
2018-12-02 19:23:50 +0300

    add bluetooth module

--- baseCode/baseCode.ino
+++ baseCode/baseCode.ino
@@ -148,3 +148,31 @@ Sim900Serial.println((char)26);
 delay(100);
 Sim900Serial.println();
 }
+
+
+
+
+//блютуз модуль
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(2, 3); // указываем пины RX и TX
+void setup()
+{
+pinMode(2,INPUT);
+pinMode(3,OUTPUT);
+Serial.begin(38400);
+mySerial.begin(9600);
+}
+void loop()
+{
+if (mySerial.available())
+{
+int c = mySerial.read(); // читаем из software-порта
+Serial.write(c); // пишем в hardware-порт
+}
+if (Serial.available())
+{
+int c = Serial.read(); // читаем из hardware-порта
+mySerial.write(c); // пишем в software-порт
+}
+}
+}
