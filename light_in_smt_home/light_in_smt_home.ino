8a72468e8ebfc6870cfe756be61a70d23cb412d2
yush <rep.yurii@gmail.com>
2018-12-26 13:40:32 +0300

    refactor code

--- light_in_smt_home/light_in_smt_home.ino
+++ light_in_smt_home/light_in_smt_home.ino
@@ -1,82 +1,90 @@
 //#include "WebServer"
 #include "DHT.h"
-#define LED1  7 //ригистрируем 7 пин как LED1 (светодиод1)
-#define LED2  8//ригистрируем 8 как LED2 (светодиод2)
-#define LED3  9//ригистрируем 9 пин как LED3 (светодиод3)
-#define PHRES1 A1//ригистрируем А1 как PHRES
-#define PHRES2 A0//ригистрируем А0 пин как PHRES2 (внутрений фоторезистор)
-#define PHRES3 A2//ригистрируем A2 как PHRES3 внешний фоторезистор
-#define TERMO A3 // пин подключения контакта
-#define WARM 6
-#define COLD 5
+#define COLD 5  // пин для подключения охлаждения
+#define WARM 6  // пин для подключения обогревателя
+#define HALL_LIGHT 7  //регистрируем 7 пин как HALL_LIGHT освещение гостинной
+#define ROOM_LIGHT 8  //регистрируем 8 как ROOM_LIGHT освещение комнаты
+#define SIGNALISATION 9  // регистрируем новый пин для сигнализации
+
+#define ROOM_PHRES A0  //регистрируем А0 пин как ROOM_PHRES фоторезистор комнаты
+#define HALL_PHRES A1  //регистрируем А1 как HALL_PHRES фоторезистор гостинной
+#define EXTERNAL_PHRES A2  //регистрируем A2 как EXTERNAL_PHRES внешний фоторезистор
+#define TERMO A3  // пин подключения датчика температуры
+#define IR_HALL A4  // пин для подключения IR датчика
+
 #define DHTTYPE DHT11
-const int analogInPin = A4;
+//const int analogInPin = A4;
 
 
 
 DHT dht(TERMO, DHTTYPE);
-int lit1;//ригистрируем переменую lit1
-int lit2;//ригистрируем переменую lit2
-int lit3;//ригистрируем переменую lit3
-int temp;//ригистрируем переменую temp
-int gas = 0 ;
+int lightHall;  // регистрируем переменую lightHall для хранения освещенности в Hall
+int lightRoom;  // регистрируем переменую lightRoom для хранения освещенности в Room
+int lightExternal;  // регистрируем переменую lightExternal для хранения освещенности снаружи
+int temp;  //регистрируем переменую temp
+int gas = 0;  // регистрируем переменную с газом
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(PHRES1,INPUT);//модифицируем пин PHRES1 (А1) как инпут (пин для приёма) 
-  pinMode(PHRES2,INPUT);//модифицируем пин PHRES2 (А0) как инпут (пин для приёма) 
-  pinMode(PHRES3,INPUT);//модифицируем пин PHRES3 (А2) как инпут (пин для приёма) 
-  pinMode(LED1, OUTPUT);//модифицируем пин LED1 (7)как аутпут()
-  pinMode(LED2, OUTPUT);//модифицируем пин LED2 (8)как аутпут()
-  pinMode(LED3, OUTPUT);//модифицируем пин LED3 (9)как аутпут()  
-  pinMode(WARM, OUTPUT);//модифицируем пин WARM (6)как аутпут()
-  pinMode(COLD, OUTPUT);//модифицируем пин COLD (5)как аутпут() 
-  pinMode(PHRES1,INPUT); 
- dht.begin();
-  Serial.begin(9600);//настраеваем монитор порта на 9600 бод
+  pinMode(HALL_PHRES,INPUT);  // модифицируем пин HALL_PHRES (А1) как инпут (пин для приёма) 
+  pinMode(ROOM_PHRES,INPUT);  // модифицируем пин ROOM_PHRES (А0) как инпут (пин для приёма) 
+  pinMode(EXTERNAL_PHRES,INPUT);  // модифицируем пин EXTERNAL_PHRES (А2) как инпут (пин для приёма) 
+  pinMode(IR_HALL, INPUT);
 
+  pinMode(SIGNALISATION, OUTPUT);  // 
+  pinMode(HALL_LIGHT, OUTPUT);  // модифицируем пин HALL_LIGHT (7)как аутпут()
+  pinMode(ROOM_LIGHT, OUTPUT);  // модифицируем пин ROOM_LIGHT (8)как аутпут()
+  pinMode(WARM, OUTPUT);  // модифицируем пин WARM (6)как аутпут()
+  pinMode(COLD, OUTPUT);  // модифицируем пин COLD (5)как аутпут() 
+  
+  dht.begin();
+  Serial.begin(9600);  // настраеваем монитор порта на 9600 бод
 }
 
 void loop() {
 
     
-  lit1 = analogRead(PHRES1);//вводим в переменую lit1 значение с пина PHRES1 (А1)
-  lit2= analogRead(PHRES2);//вводим в переменую lit2 значение с пина PHRES2 (А0)
-  lit3= analogRead(PHRES3);//вводим в переменую lit3 значение с пина PHRES3 (А2)
-  gas = analogRead (analogInPin);
-  float temp= dht.readTemperature();//
+  lightHall = analogRead(HALL_PHRES);  // сохраняем в переменую lightHall значение считанное с пина HALL_PHRES (А1)
+  lightRoom= analogRead(ROOM_PHRES);  // сохраняем в переменую lightRoom значение считанное с пина ROOM_PHRES (А0)
+  lightExternal= analogRead(EXTERNAL_PHRES);  // сохраняем в переменую lightExternal значение считанное с пина EXTERNAL_PHRES (А2)
+  irhall = analogRead(IR_HALL);  // сохраняем в переменную значение считанное с ИК датчика
 
- //Serial.print(lit1);//пичатаем в монитор порта переменую lit1
-//  Serial.print(lit1);//пичатаем в монитор порта переменую lit1
-//  Serial.print(' ');//пичатаем в монитор порта пробел
- Serial.println( gas);//пичатаем в монитор порта переменую lit2
-//  Serial.println(temp);/пичатаем в монитор порта переменую templit2
-  //  Serial.print(' ');//пичатаем в монитор порта пробел
-  // Serial.println(lit3);//пичатаем в монитор порта переменую lit3
-  //digitalWrite(LED1,lit1);
-  delay(500);//ждём 0.5 сикунд
-// проверяем освещение снаружи
-  if (lit3<200){
-    digitalWrite(LED2,HIGH);
-    digitalWrite(LED1,HIGH);}
-  if (lit3>200){
-    digitalWrite(LED2,LOW);
-    digitalWrite(LED1,LOW);}
-  // проверяем освещение  в комнатах 
-  // дальняя комната
-  if (lit2<200){digitalWrite(LED2,HIGH);}
-  if (lit2>200){digitalWrite(LED2,LOW);}  
-  //Ближняя комната
-  if (lit1<200){digitalWrite(LED1,HIGH);} // TODO изменить границы.НЕ НАДО
-  if (lit1>200){digitalWrite(LED1,LOW);}
+  // Условие присутствия
+  if (irhall< 10){
+    
+  }
+  
+  temp= dht.readTemperature();  // сохраняем в переменную temp показания температуры считанное с DHT датчика
+  
+  Serial.println( gas);  // печатаем в монитор порта переменую
 
-// условие для включения нагревания и выключения охлаждения
-  if (temp<22.00){
-    digitalWrite(WARM, HIGH);// TODO
-    digitalWrite(COLD,LOW);}// TODO
-// условие для выключения нагревания и включения охлаждения
- if (temp>=22.00){
-    digitalWrite(COLD,HIGH);// TODO
+  
+  
+  // Условие для включения/выключения света в Hall
+  // Темно в комнате
+  if (lightHall<=200) digitalWrite(HALL_LIGHT,HIGH);
+  // В противном случае свет в комнате надо погасить
+  else digitalWrite(HALL_LIGHT,LOW);
+  
+  // Условие для включения/выключения света в Room
+  // Темно в комнате
+  if (lightRoom<=200) digitalWrite(ROOM_LIGHT,HIGH);
+  // В противном случае свет в комнате надо погасить
+  else digitalWrite(ROOM_LIGHT,LOW); 
+
+  // условие для включения нагревания и выключения охлаждения
+  if (temp<21){
+    digitalWrite(WARM, HIGH);  // TODO
+    digitalWrite(COLD,LOW);
+    }  // TODO
+// условие для выключения нагревания и охлаждения
+  else if (temp>=21 && temp<25) {
+    digitalWrite(COLD,LOW);// TODO
     digitalWrite(WARM,LOW);}// TODO
- if ( gas >= 750);
+// условие для выключения нагревателя и включения охлажения
+  else:{
+    digitalWrite(COLD,LOW);// TODO
+    digitalWrite(WARM,LOW);}// TODO
+  
+
+  delay(500);  // ждём 0.5 сeкунд
 }
